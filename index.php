3a6a31b018afd8e28630ea99567f8bc993037335
banjoquint123 <65598283+banjoquint123@users.noreply.github.com>
2023-08-20 12:20:36 +0100

    changes to footer and header defs

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!---This is an initial stab at using Bootstrap 4 on a project. The initial idea is to sort out a database, get connected, and using forms and modals process some information in a CRUD way. -->
 <!-- Then do the same in Bootstrap 5 -->
+
 <?php 
+  $title= 'Index';
   require_once 'includes/header.php';
 ?>
     <h1>Hello, world!</h1>
