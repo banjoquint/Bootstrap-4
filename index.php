2f653210b77d5fea6c906a8ef5254919d6e54efe
banjoquint123 <65598283+banjoquint123@users.noreply.github.com>
2023-08-20 10:05:50 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!---This is an initial stab at using Bootstrap 4 on a project. The initial idea is to sort out a database, get connected, and using forms and modals process some information in a CRUD way. -->
+<!-- Then do the same in Bootstrap 5 -->
+<?php 
+  require_once 'includes/header.php';
+?>
+    <h1>Hello, world!</h1>
+<?php 
+    require_once 'includes/footer.php';
+ ?>
