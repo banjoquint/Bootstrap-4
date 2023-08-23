dd7b37a69460bff092970edfc9ed426e67fc4713
banjoquint123 <65598283+banjoquint123@users.noreply.github.com>
2023-08-23 18:37:11 +0100

    changed function column to email

--- child-list.php
+++ child-list.php
@@ -10,7 +10,7 @@
       <th scope="col">#</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Email</th>
     </tr>
   </thead>
   <tbody>
